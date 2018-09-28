f85608a181ec1fa629572afd2e6a1d00c113edea
林放 <linfang@linfangdeMacBook-Pro.local>
2018-09-28 13:53:18 +0800

    设备添加界面，增加二次确认界面

--- vue-admin-template/src/views/device/device_add.vue
+++ vue-admin-template/src/views/device/device_add.vue
@@ -2,6 +2,10 @@
   <div class="app-container">
     <el-form ref="deviceAddForm" :rules="rules" :model="form" label-width="120px">
       <el-row>
+        注意1，请首先在ERP中，基础字典->仓库，添加设备，并勾选面膜机
+        注意2，正常流程下，应该在售货机上做售货机添加的操作，本页面作为后备使用
+      </el-row>
+      <el-row>
         <el-col :span="8">
           <el-form-item label="设备淘宝编号" prop="deviceTaobaoNo">
             <el-input v-model="form.deviceTaobaoNo"></el-input>
@@ -18,10 +22,21 @@
       <!-- </el-form-item> -->
       
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">新建</el-button>
+        <el-button type="primary" @click="showSubmitForm">新建</el-button>
       </el-form-item>
     </el-form>
+    <el-dialog title="添加售货机" :visible.sync="confirmFormVisible">
+      <el-form ref="confirmForm" :model="confirmForm" :rules="confirmRules">
+        <el-form-item label="请输入<确认添加>以确认添加售货机" label-width="300px" prop="name">
+          <el-input v-model="confirmForm.name" autocomplete="off"></el-input>
+        </el-form-item>
 
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="confirmFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="onSubmit">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -38,7 +53,9 @@ export default {
         "deviceTaobaoNo": null,  //设备淘宝编号
         "shopId": null, //淘宝门店号
       },
- 
+      confirmForm : {
+        name : ''
+      },
       rules:{
         // deviceTaobaoNo: [
         //     { required: true, message: '请选择机器ID', trigger: 'change' },
@@ -51,6 +68,14 @@ export default {
         ],
         
       },
+      confirmRules : {
+        name: [
+            { required: true, message: '请输入文字', trigger: 'change' },           
+            { validator: this.checkConfirmForm, trigger: 'change' },
+
+        ],
+      },
+      confirmFormVisible : false,
       
     }
   },
@@ -59,8 +84,19 @@ export default {
     
   },
   methods: {
+    showSubmitForm(){
+      let that = this;
+      this.$refs['deviceAddForm'].validate((valid) => {
+          if (valid) {
+            that.confirmFormVisible = true;
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+    },
     onSubmit() {
-        this.$refs['deviceAddForm'].validate((valid) => {
+        this.$refs['confirmForm'].validate((valid) => {
           if (valid) {
             this.$message('正在保存')
             this.addDevice();
@@ -83,6 +119,7 @@ export default {
         .then(function(res){
           console.log(res);
           if(res.data !== false){
+            that.confirmFormVisible = false;
             that.$message({
               message: '添加设备成功，请到售货机页面查看!',
               type: 'success'
@@ -100,7 +137,14 @@ export default {
 
         })
     },
-    
+    checkConfirmForm(rule, value, callback){
+        // console.log(arguments);
+        // debugger
+        if(value != '确认添加'){
+              callback(new Error('请输入正确的文字'));
+            }
+        callback();
+      },
   }
 }
 </script>
